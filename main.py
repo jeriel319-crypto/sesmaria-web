6e1db73280ef3a0f6a912645e588ec40a82eb5e3
jeriel319-crypto <jeriel319@gmail.com>
2026-05-07 19:21:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,44 +6,39 @@ st.set_page_config(page_title="Sesmaria do Cerro - Doações", layout="wide")
 st.title("🚜 Sesmaria do Cerro - Sistema de Doações")
 st.write("Gerencie as doações e retiradas de alimentos da região.")
 
-# Lista de produtos regionalizada
+# Lista de produtos confirmados e estáveis (Mandioca, Feijão e Maracujá removidos)
 produtos_info = {
     "Beterraba": "kg", "Abacaxi": "unid", "Cebola": "kg", "Batata": "kg", 
-    "Mandioca": "kg", "Chuchu": "unid", "Maracujá": "unid", "Laranja": "kg", 
-    "Maçã": "kg", "Banana": "kg", "Melancia": "unid", "Mamão": "unid", 
-    "Cenoura": "kg", "Tomate": "unid", "Alface": "unid", "Repolho": "unid", 
-    "Abóbora": "unid", "Pimentão": "unid", "Alho": "kg", "Milho": "unid", 
-    "Feijão": "kg", "Amendoim": "kg"
+    "Laranja": "kg", "Maçã": "kg", "Banana": "kg", "Melancia": "unid", 
+    "Mamão": "unid", "Cenoura": "kg", "Tomate": "unid", "Alface": "unid", 
+    "Repolho": "unid", "Abóbora": "unid", "Pimentão": "unid", "Alho": "kg", 
+    "Milho": "unid", "Amendoim": "kg"
 }
 
 # Inicialização do estoque
 if 'estoque' not in st.session_state:
     st.session_state.estoque = {produto: 0 for produto in produtos_info.keys()}
 
-# Dicionário de imagens - Links substituídos por fontes mais estáveis
+# Dicionário de imagens que estavam funcionando perfeitamente
 imagens = {
-    "Beterraba": "https://cdn-icons-png.flaticon.com/512/2909/2909841.png",
-    "Abacaxi": "https://cdn-icons-png.flaticon.com/512/2153/2153711.png",
-    "Cebola": "https://cdn-icons-png.flaticon.com/512/2316/2316629.png",
-    "Batata": "https://cdn-icons-png.flaticon.com/512/1135/1135544.png",
-    "Mandioca": "https://cdn-icons-png.flaticon.com/512/6122/6122558.png", # Link reforçado
-    "Chuchu": "https://cdn-icons-png.flaticon.com/512/10046/10046903.png",
-    "Maracujá": "https://cdn-icons-png.flaticon.com/512/13601/13601614.png", # Link reforçado
-    "Laranja": "https://cdn-icons-png.flaticon.com/512/1728/1728739.png",
-    "Maçã": "https://cdn-icons-png.flaticon.com/512/415/415733.png",
-    "Banana": "https://cdn-icons-png.flaticon.com/512/2909/2909761.png",
-    "Melancia": "https://cdn-icons-png.flaticon.com/512/2153/2153724.png",
-    "Mamão": "https://cdn-icons-png.flaticon.com/512/2153/2153710.png",
-    "Cenoura": "https://cdn-icons-png.flaticon.com/512/1041/1041355.png",
-    "Tomate": "https://cdn-icons-png.flaticon.com/512/1202/1202125.png",
-    "Alface": "https://cdn-icons-png.flaticon.com/512/2153/2153676.png",
-    "Repolho": "https://cdn-icons-png.flaticon.com/512/2347/2347035.png",
-    "Abóbora": "https://cdn-icons-png.flaticon.com/512/2153/2153718.png",
-    "Pimentão": "https://cdn-icons-png.flaticon.com/512/2153/2153702.png",
-    "Alho": "https://cdn-icons-png.flaticon.com/512/2153/2153674.png",
-    "Milho": "https://cdn-icons-png.flaticon.com/512/2153/2153664.png",
-    "Feijão": "https://cdn-icons-png.flaticon.com/512/8157/8157580.png", # Link reforçado
-    "Amendoim": "https://cdn-icons-png.flaticon.com/512/2346/2346944.png"
+    "Beterraba": "https://img.icons8.com/color/144/beet.png",
+    "Abacaxi": "https://img.icons8.com/color/144/pineapple.png",
+    "Cebola": "https://img.icons8.com/color/144/onion.png",
+    "Batata": "https://img.icons8.com/color/144/potato.png",
+    "Laranja": "https://img.icons8.com/color/144/orange.png",
+    "Maçã": "https://img.icons8.com/color/144/apple.png",
+    "Banana": "https://img.icons8.com/color/144/banana.png",
+    "Melancia": "https://img.icons8.com/color/144/watermelon.png",
+    "Mamão": "https://img.icons8.com/color/144/papaya.png",
+    "Cenoura": "https://img.icons8.com/color/144/carrot.png",
+    "Tomate": "https://img.icons8.com/color/144/tomato.png",
+    "Alface": "https://img.icons8.com/color/144/lettuce.png",
+    "Repolho": "https://img.icons8.com/color/144/cabbage.png",
+    "Abóbora": "https://img.icons8.com/color/144/pumpkin.png",
+    "Pimentão": "https://img.icons8.com/color/144/paprika.png",
+    "Alho": "https://img.icons8.com/color/144/garlic.png",
+    "Milho": "https://img.icons8.com/color/144/corn.png",
+    "Amendoim": "https://img.icons8.com/color/144/peanuts.png"
 }
 
 # Exibição do Estoque
@@ -52,9 +47,7 @@ cols = st.columns(4)
 for i, (item, qtd) in enumerate(st.session_state.estoque.items()):
     unidade = produtos_info[item]
     with cols[i % 4]:
-        # Tenta carregar a imagem, se falhar coloca um ícone de caixa
-        url_img = imagens.get(item, "https://cdn-icons-png.flaticon.com/512/679/679821.png")
-        st.image(url_img, width=65)
+        st.image(imagens[item], width=65)
         st.metric(label=item, value=f"{qtd} {unidade}")
 
 st.divider()
@@ -84,5 +77,4 @@ with col2:
             st.rerun()
         else:
             st.error("Estoque insuficiente!")
-
-st.sidebar.info("Sistema desenvolvido para o projeto acadêmico de ADS.")
+            
